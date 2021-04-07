aed6596d5231464a2bf2a47dea61c87e37fbd5ef
kwameasare04 <73545744+kwameasare04@users.noreply.github.com>
2021-04-07 18:17:15 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const myNumbers = [123, 50, 27];
+
+const pushIfUnique = (inputArray, newElement) => {
+if(inputArray.includes(newElement) === true){
+    console.log( newElement + " is already in the array")
+} else
+inputArray.push(newElement);
+};
+
+
+
+
+
+/* Create a function "pushIfUnique" with two parameters "inputArray" and "newElement".
+
+If "inputArray" already contains "newElement" print "{newElement} is already in the array" to the console.
+
+Otherwise push "newElement" to the "inputArray". 
+
+NOTE: We assume that "inputArray" may contain only primitive variables types
+*/
+
+pushIfUnique(myNumbers, 50); // "50 is already in the array"
+console.log(myNumbers); // [123, 50, 27]
+
+pushIfUnique(myNumbers, 80);
+console.log(myNumbers); // [123, 50, 27, 80]
+
+pushIfUnique(myNumbers, 80); // "80 is already in the array"
+console.log(myNumbers); // [123, 50, 27, 80]
+
+pushIfUnique(myNumbers, 77);
+console.log(myNumbers); // [123, 50, 27, 80, 77]
